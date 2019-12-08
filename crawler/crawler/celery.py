843ef50b9de2f129c011af62395408063db7985b
augan93 <auganenu@gmail.com>
2019-12-09 00:13:50 +0600

    Запуск каждын 30 мин

--- crawler/crawler/celery.py
+++ crawler/crawler/celery.py
@@ -17,7 +17,7 @@ app.autodiscover_tasks()
 app.conf.beat_schedule = {
     'get-news-every-30-minutes': {
         'task': 'news.tasks.get_news',
-        'schedule': 5,  # execute every 1800 second
+        'schedule': 1800,  # execute every 30 min
     },
 }
 app.conf.timezone = 'UTC'
